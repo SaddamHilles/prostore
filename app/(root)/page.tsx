5078e4ecb0ebad611cdc2fb3265b0c399083cda8
SaddamHilles <sdm.hilles@gmail.com>
2025-01-17 03:03:21 +0200

    product cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import ProductList from '@/components/product/product-list';
+import sampleData from '@/db/sample-data';
+
 const Homepage = () => {
   return (
     <div>
-      <h3>Prostore</h3>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
     </div>
   );
 };
---
+++ components/product/product-list/index.tsx
@@ -0,0 +1,28 @@
+import { Product } from '@/lib/types/types';
+import ProductCard from '../product-card';
+
+interface Props {
+  data: Product[];
+  title?: string;
+  limit?: number;
+}
+const ProductList = ({ data, title, limit = data.length }: Props) => {
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {data.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {data.slice(0, limit).map(product => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No Products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
