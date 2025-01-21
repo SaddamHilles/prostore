73b8d81ddf131e3704509c048f0b4ae5a5b81f0e
SaddamHilles <sdm.hilles@gmail.com>
2025-01-21 12:25:42 +0200

    next auth models

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'utfs.io', port: '' }],
+  },
   /* config options here */
 };
 
